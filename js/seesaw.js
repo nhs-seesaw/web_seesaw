5d957fe1d47399401caa09e2dcbbb064c5b5e8b7
Connell Locke <clocke01@gmail.com>
2015-06-03 12:05:16 +0100

    - Removed percentage alert indicator and percentage graphic at bottom of   webapp. The alert was for development purposes only, the graphic was   confusing.
    - Changed goal display area to columns instead of circles, as evidence
      shows humans are better able to estimate relative magnitude of column
      height vs object area. Columns were chosen over horizontal bars in this
      build as they were easier to implement, and correspond exactly to what
      appears on the seesaw.
    
    - Added goal labels to x-axis position area
    
    - Added small label showing neutral point of x-axis
    
    - Added Key to seesaw area with names of goals + colour
    
    - Goals more across seesaw in 'live' if the position indicator is changed.
    
    - Created second webpage to demonstrate layout consequences of increasing the
      number of position options on the seesaw:
    
        -seesaw.html = 17 possible x-axis positions (up from 9), as a result
         the width of each goal is narrower (-8 to +8).
    
        -seesaw_2.html = 11 possible x-axis positions (up from 9), again the
         result is that each goal is narrower on the seesaw. (-5 to +5).
    
    The cause for this width issue is simply how the seesaw is created using
    HTML and css - adding more positions makes the goals narrower, and
    makes the seesaw look more sparse.  Until a way around this limitation is
    found, I would favour, the -5 to +5 option (or less) -- the tool remains
    functional and flexible conceptually, and still works visually. This is a
    discussion point for the team.
    
    Known Issues:
    - When importance indicator is set to zero goal column doesn't entirely
      disappear
    
    - App displays 5 goal options with sliders at all times, this should be
      changed so that goals can be added and removed dynamically
    
    - Need to work out a way to show seesaw movement when swing is subtle.
    
    Other Issues:
    
    - As project moves towards a viable beta, we should consider adding an option
      for 'showing working out'. Something like a collapsed text area, not visible
      by default, but which can show clinicans (and patients), how the seesaw
      came to rest where it did, and what the numbers actually are. I'm thinking
      here of sites like those that tell you who you should vote for based on
      a quiz-- they give a suggested answer, and then if you wish, break down
      how they arrived at that answer in a separate area with numbers and further
      graphics.

--- js/seesaw.js
+++ js/seesaw.js
@@ -20,14 +20,14 @@ $(document).ready(function(){
 
 
    //Crude:
-   //Function for each of the 4 importance sliders- changes size of goals
+   //Function for each of the 5 importance sliders- changes size of goals
     
     
     
     $('#imp1').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal1_circle").css("width" , h +"em");
+            //$("#goal1_circle").css("width" , h +"em");
             $("#goal1_circle").css("height" , h +"em");
             $("#goal1_circle").css("line-height", h + "em");
 
@@ -36,7 +36,7 @@ $(document).ready(function(){
     $('#imp2').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal2_circle").css("width" , h +"em");
+            //$("#goal2_circle").css("width" , h +"em");
             $("#goal2_circle").css("height" , h +"em");
             $("#goal2_circle").css("line-height", h + "em");
   
@@ -45,7 +45,7 @@ $(document).ready(function(){
     $('#imp3').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal3_circle").css("width" , h +"em");
+            //$("#goal3_circle").css("width" , h +"em");
             $("#goal3_circle").css("height" , h +"em");
             $("#goal3_circle").css("line-height", h + "em");
 
@@ -55,7 +55,7 @@ $(document).ready(function(){
     $('#imp4').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal4_circle").css("width" , h +"em");
+            //$("#goal4_circle").css("width" , h +"em");
             $("#goal4_circle").css("height" , h +"em");
             $("#goal4_circle").css("line-height", h + "em");
          
@@ -64,21 +64,13 @@ $(document).ready(function(){
     $('#imp5').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal5_circle").css("width" , h +"em");
+            //$("#goal5_circle").css("width" , h +"em");
             $("#goal5_circle").css("height" , h +"em");
             $("#goal5_circle").css("line-height", h + "em");
          
         }
     });
-    $('#imp6').slider({
-        formatter: function(value) {
-            h = 1*value/2;
-            $("#goal6_circle").css("width" , h +"em");
-            $("#goal6_circle").css("height" , h +"em");
-            $("#goal6_circle").css("line-height", h + "em");
-         
-        }
-    });
+
     
     
 
@@ -99,6 +91,31 @@ $(document).ready(function(){
     $("#positiongoalsbutton").on("click", function(){
         $("#positiongoalsbutton").hide();
         $("#balancearea").show();
+        
+        //Get Goal Text
+        g1_text = $("input[name='g1_text']").val();
+        g2_text = $("input[name='g2_text']").val();
+        g3_text = $("input[name='g3_text']").val();
+        g4_text = $("input[name='g4_text']").val();
+        g5_text = $("input[name='g5_text']").val();
+        
+        //Iterate through goal_texts and create
+        
+        g_text= [g1_text, g2_text, g3_text, g4_text, g5_text];
+        size = g_text.length;
+        
+        for(i=0; i < size; i++){
+            g = i+1;
+            if(g_text[i].length !==0){
+                $("#pos_"+g).children(".bal_label").empty();
+                $("#pos_"+g).children(".bal_label").append(g_text[i]);
+            }
+            
+        };
+        
+        
+        
+        
         $("html, body").animate({
             scrollTop: $("#balancearea").offset().top}, 2000);
         
@@ -119,6 +136,39 @@ $(document).ready(function(){
         $("#showseesawbutton").show();
         
     });
+    
+    //'Live' positioning of Blocks on seesaw
+    
+    $('#bal1').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal2').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal3').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal4').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal5').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    
+    
+    
+    
+    
 
     //Initialise Sliders:
     $('.balslider').slider();
@@ -137,8 +187,9 @@ $(document).ready(function(){
     $("#balancebutton").on("click", function(){
         drawseesaw();
         balanceseesaw();
-        $("#balchart").fadeIn(7000);
     });
+    
+    
     
     function drawseesaw(){
         
@@ -154,8 +205,31 @@ $(document).ready(function(){
                 //Set height of newly created goal block
                 $("#g"+i+"_block").css("height", v +"em");
                 $("#g"+i+"_block").css("line-height", v +"em");
+                
+                
+                //Add Key Label
+                $("#g"+i+"_key").show();
+                label = $("input[name='g"+i+"_text']").val();
+                
+                //ensure not null
+                if(label.length < 1)
+                {
+                    $("#g"+i+"_key").children(".key_text").empty();
+                    $("#g"+i+"_key").children(".key_text").append("Goal " + i);
+                }
+                else
+                {
+                    $("#g"+i+"_key").children(".key_text").empty();
+                    $("#g"+i+"_key").children(".key_text").append(label);
+                }
+                
+            }
+            
+            if(v <1){
+                $("#g"+i+"_key").hide();
             }
         }
+        
         
         
         
@@ -188,65 +262,15 @@ $(document).ready(function(){
         moment = g1+g2+g3+g4+g5;
         percentage = (moment / total_importance)*100;
         
-        alert("% Swing = "+percentage);
         
         //Set Seesaw tilt - to a maximum of 40 degrees
         tilt = percentage/2.5;
-        
-        
         
         $("#seesaw").css("transform-origin", "50% 100%");
         $("#seesaw").css("transform", "rotate("+tilt+"deg)");
         
         
-        //Bottom Balance Chart:
-        
-        //Calculate widths
-        if(percentage !== 0){
-            width_1 = 50+percentage;
-            width_2 = 100 - width_1;
-        }
-        else{
-            width_1 = 50;
-            width_2 = 50;
-        }
-        
-        
-        //Round Number for printing to screen
-        function roundToTwo(num) {    
-            return +(Math.round(num + "e+2")  + "e-2");
-        }
-        
-        width_1b = roundToTwo(width_1);
-        width_2b = roundToTwo(width_2);
-        
-        //Get Option Names
-        op1 = $("input[name='op1']").val();
-        op2 = $("input[name='op2']").val();
-        
-        //Empty bars
-        $("#op1_bar").empty();
-        $("#op2_bar").empty();
-        
-        //If Option names set- show these with a space
-        if(op1.length !== 0 && op2.length !==0){
-            $("#op1_bar").append(op1+":&nbsp; ");
-            $("#op1_bar").append(op2+":&nbsp; ");
-        }
-        //Else use 'option 1' and 'option 2'
-        else{
-            $("#op1_bar").append("Option 1:&nbsp; ");
-            $("#op2_bar").append("Option 2:&nbsp; ");
-        }
-        
-        //append % 
-        $("#op1_bar").append(width_2b + "%");
-        $("#op2_bar").append(width_1b + "%");
-        
-        //Set Width
-        $("#op1_bar").css("width", width_2+"%");
-        $("#op2_bar").css("width", width_1+"%");
-        
+          
     };
     
     
--- js/seesaw_2.js
+++ js/seesaw_2.js
@@ -20,68 +20,60 @@ $(document).ready(function(){
 
 
    //Crude:
-   //Function for each of the 4 importance sliders- changes size of goals
+   //Function for each of the 5 importance sliders- changes size of goals
     
     
     
     $('#imp1').slider({
         formatter: function(value) {
             h = 1*value/2;
-            //Diameter
-            $("#goal1_circle").css("width" , h +"em");
+            //$("#goal1_circle").css("width" , h +"em");
             $("#goal1_circle").css("height" , h +"em");
             $("#goal1_circle").css("line-height", h + "em");
-            
-            //Area of Circle
-            /*
- 
-            Area = A    Value = V   Diameter = D
-            
-            A = (PI*D^2)/4;     D = Sqrt(A*4/PI)
-            
-            Slider Value = V    Max Value = Vmax
-            
-            Max Diameter = Dmax (set to 12)
-            MaxArea = (PI * Dmax^2)/4
-            
-            For Value V:
-            
-            D^2 = (V * 4 * ((PI*Dmax^2)/4) ) / (Vmax * PI) )
-                = (V * PI * Dmax^2) / (Vmax * PI)
-                = (V * Dmax^2) / Vmax
-            
-            Therefore if Dmax = 12 em, and Vmax = 24:
-            
-            D = Sqrt( (V * 144)/ 24)
-            
-            
-            */
-            
-            d = Math.sqrt(value*(144)/24);
-            
-            $("#goal1_circle_a").css("width" , d +"em");
-            $("#goal1_circle_a").css("height" , d +"em");
-            $("#goal1_circle_a").css("line-height", d + "em");
 
         }
     });
     $('#imp2').slider({
         formatter: function(value) {
             h = 1*value/2;
-            $("#goal2_circle").css("width" , h +"em");
+            //$("#goal2_circle").css("width" , h +"em");
             $("#goal2_circle").css("height" , h +"em");
             $("#goal2_circle").css("line-height", h + "em");
-            
-            //Area
-            d = Math.sqrt(value*(144)/24);
-            $("#goal2_circle_a").css("width" , d +"em");
-            $("#goal2_circle_a").css("height" , d +"em");
-            $("#goal2_circle_a").css("line-height", d + "em");
   
         }
     });
-  
-   
+    $('#imp3').slider({
+        formatter: function(value) {
+            h = 1*value/2;
+            //$("#goal3_circle").css("width" , h +"em");
+            $("#goal3_circle").css("height" , h +"em");
+            $("#goal3_circle").css("line-height", h + "em");
+
+        }
+    });
+    
+    $('#imp4').slider({
+        formatter: function(value) {
+            h = 1*value/2;
+            //$("#goal4_circle").css("width" , h +"em");
+            $("#goal4_circle").css("height" , h +"em");
+            $("#goal4_circle").css("line-height", h + "em");
+         
+        }
+    });
+    $('#imp5').slider({
+        formatter: function(value) {
+            h = 1*value/2;
+            //$("#goal5_circle").css("width" , h +"em");
+            $("#goal5_circle").css("height" , h +"em");
+            $("#goal5_circle").css("line-height", h + "em");
+         
+        }
+    });
+
+    
+    
+
 
     //Draw Goals - shows circles + balance goals button
     $("#drawgoalsbutton").on("click", function(){
@@ -99,12 +91,37 @@ $(document).ready(function(){
     $("#positiongoalsbutton").on("click", function(){
         $("#positiongoalsbutton").hide();
         $("#balancearea").show();
+        
+        //Get Goal Text
+        g1_text = $("input[name='g1_text']").val();
+        g2_text = $("input[name='g2_text']").val();
+        g3_text = $("input[name='g3_text']").val();
+        g4_text = $("input[name='g4_text']").val();
+        g5_text = $("input[name='g5_text']").val();
+        
+        //Iterate through goal_texts and create
+        
+        g_text= [g1_text, g2_text, g3_text, g4_text, g5_text];
+        size = g_text.length;
+        
+        for(i=0; i < size; i++){
+            g = i+1;
+            if(g_text[i].length !==0){
+                $("#pos_"+g).children(".bal_label").empty();
+                $("#pos_"+g).children(".bal_label").append(g_text[i]);
+            }
+            
+        };
+        
+        
+        
+        
         $("html, body").animate({
             scrollTop: $("#balancearea").offset().top}, 2000);
         
         
-         
-       //Show Option Labels
+        
+        //Show Option Labels
         op1 = $("input[name='op1']").val();
         op2 = $("input[name='op2']").val();
         
@@ -119,6 +136,39 @@ $(document).ready(function(){
         $("#showseesawbutton").show();
         
     });
+    
+    //'Live' positioning of Blocks on seesaw
+    
+    $('#bal1').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal2').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal3').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal4').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    $('#bal5').slider({
+        formatter: function(value) {
+            drawseesaw();
+        }
+    });
+    
+    
+    
+    
+    
 
     //Initialise Sliders:
     $('.balslider').slider();
@@ -137,8 +187,9 @@ $(document).ready(function(){
     $("#balancebutton").on("click", function(){
         drawseesaw();
         balanceseesaw();
-        $("#balchart").fadeIn(7000);
     });
+    
+    
     
     function drawseesaw(){
         
@@ -154,8 +205,31 @@ $(document).ready(function(){
                 //Set height of newly created goal block
                 $("#g"+i+"_block").css("height", v +"em");
                 $("#g"+i+"_block").css("line-height", v +"em");
+                
+                
+                //Add Key Label
+                $("#g"+i+"_key").show();
+                label = $("input[name='g"+i+"_text']").val();
+                
+                //ensure not null
+                if(label.length < 1)
+                {
+                    $("#g"+i+"_key").children(".key_text").empty();
+                    $("#g"+i+"_key").children(".key_text").append("Goal " + i);
+                }
+                else
+                {
+                    $("#g"+i+"_key").children(".key_text").empty();
+                    $("#g"+i+"_key").children(".key_text").append(label);
+                }
+                
+            }
+            
+            if(v <1){
+                $("#g"+i+"_key").hide();
             }
         }
+        
         
         
         
@@ -188,65 +262,15 @@ $(document).ready(function(){
         moment = g1+g2+g3+g4+g5;
         percentage = (moment / total_importance)*100;
         
-        alert("% Swing = "+percentage);
         
         //Set Seesaw tilt - to a maximum of 40 degrees
         tilt = percentage/2.5;
-        
-        
         
         $("#seesaw").css("transform-origin", "50% 100%");
         $("#seesaw").css("transform", "rotate("+tilt+"deg)");
         
         
-        //Bottom Balance Chart:
-        
-        //Calculate widths
-        if(percentage !== 0){
-            width_1 = 50+percentage;
-            width_2 = 100 - width_1;
-        }
-        else{
-            width_1 = 50;
-            width_2 = 50;
-        }
-        
-        
-        //Round Number for printing to screen
-        function roundToTwo(num) {    
-            return +(Math.round(num + "e+2")  + "e-2");
-        }
-        
-        width_1b = roundToTwo(width_1);
-        width_2b = roundToTwo(width_2);
-        
-        //Get Option Names
-        op1 = $("input[name='op1']").val();
-        op2 = $("input[name='op2']").val();
-        
-        //Empty bars
-        $("#op1_bar").empty();
-        $("#op2_bar").empty();
-        
-        //If Option names set- show these with a space
-        if(op1.length !== 0 && op2.length !==0){
-            $("#op1_bar").append(op1+":&nbsp; ");
-            $("#op1_bar").append(op2+":&nbsp; ");
-        }
-        //Else use 'option 1' and 'option 2'
-        else{
-            $("#op1_bar").append("Option 1:&nbsp; ");
-            $("#op2_bar").append("Option 2:&nbsp; ");
-        }
-        
-        //append % 
-        $("#op1_bar").append(width_2b + "%");
-        $("#op2_bar").append(width_1b + "%");
-        
-        //Set Width
-        $("#op1_bar").css("width", width_2+"%");
-        $("#op2_bar").css("width", width_1+"%");
-        
+          
     };
     
     
